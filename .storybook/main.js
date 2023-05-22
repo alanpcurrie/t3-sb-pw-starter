5a863c95570b81249bc059f40b848478a8398afa
Alan P Currie <alanc909@outlook.com>
2023-05-22 09:12:20 +0100

    json output config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,22 @@
-const path = require('path');
+const path = require('path')
 module.exports = {
-  "stories": ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-a11y", "@storybook/addon-mdx-gfm"],
-  "framework": {
-    "name": "@storybook/nextjs",
-    "options": {}
-  },
-  "docs": {
-    autodocs: "tag"
-  }
-};
+    stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+        '@storybook/addon-a11y',
+        '@storybook/addon-mdx-gfm',
+    ],
+    framework: {
+        name: '@storybook/nextjs',
+        options: {},
+    },
+    docs: {
+        autodocs: 'tag',
+    },
+    features: {
+        buildStoriesJson: true,
+        storyStoreV7: true,
+    },
+}
